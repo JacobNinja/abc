0c5730d61ea6a031130ee67fa1f09f9f8012c8c7
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-03-05 22:09:13 -0700

    Add main function for running abc metrics on ruby files

--- src/abc/core.clj
+++ src/abc/core.clj
@@ -1,11 +1,19 @@
-(ns abc.core)
+(ns abc.core
+  (:require [abc.extractors.ruby :refer [parse]]))
 
-(defn metric-result [rs]
+(defn metric-result [r]
   (fn [metric]
-    (let [metric-result (get-in rs [:metrics metric] 0)]
+    (let [metric-result (count (get-in r [:metrics metric] '()))]
       (* metric-result metric-result))))
 
-(defn abc [rs]
-  (Math/sqrt (apply +
-                    (map (metric-result rs)
-                         [:branches :assignments :conditionals]))))
+(defn abc [r]
+  (assoc r :score
+    (int (Math/sqrt (apply +
+                      (map (metric-result r)
+                           [:branches :assignments :conditionals]))))))
+
+(defn abcs [rs]
+  (map abc rs))
+
+(defn main [file]
+  (println (abcs (parse (slurp file)))))
--- test/abc/core_test.clj
+++ test/abc/core_test.clj
@@ -2,9 +2,21 @@
   (:require [clojure.test :refer :all]
             [abc.core :refer :all]))
 
+(defn generate-data [n]
+  (repeat n {:line 2 :range [3 5]}))
+
 (deftest abc-test
   (testing "metric"
-    (is (= (abc {:method "foo" :metrics {:conditionals 2
-                                          :branches 5
-                                          :assignments 10}}))
-           11)))
+    (is (= (:score (abc {:method "foo" :metrics {:conditionals (generate-data 2)
+                                          :branches (generate-data 5)
+                                          :assignments (generate-data 10)}}))
+           11))
+    (is (= (map :score (abcs [{:method "foo"
+                               :metrics {:conditionals (generate-data 2)
+                                         :branches (generate-data 5)
+                                         :assignments (generate-data 10)}}
+                              {:method "bar"
+                               :metrics {:conditionals (generate-data 2)
+                                         :branches (generate-data 5)
+                                         :assignments (generate-data 10)}}]))
+           [11 11]))))
